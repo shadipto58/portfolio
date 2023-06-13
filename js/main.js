507d2aaec1235bef6775698a3a60f05a8b13c785
Shadipto Mojumder <118707478+shadipto58@users.noreply.github.com>
2023-06-13 16:30:28 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+
+
+	
+
+
+	$('.one-time').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 1000,
+	  autoplay: true,
+	  slidesToShow: 1,
+	  adaptiveHeight: true
+	});
+
+
+   
+
+
+
+
+
+
+
+
